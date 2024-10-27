cd34a8f0bfbc41c10dd3bd564cddf7034a644365
Kieran David Evans <keyz182@gmail.com>
2024-10-27 08:34:57 +0000

    Cookie cutter initial commit

---
+++ 1.5/Source/Keyz'_Misc_Resources/Keyz'_Misc_ResourcesDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Keyz'_Misc_Resources;
+
+[DefOf]
+public static class Keyz'_Misc_ResourcesDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static Keyz'_Misc_ResourcesDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(Keyz'_Misc_ResourcesDefOf));
+}
---
+++ 1.5/Source/Keyz'_Misc_Resources/Keyz'_Misc_ResourcesMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Keyz'_Misc_Resources;
+
+public class Keyz'_Misc_ResourcesMod : Mod
+{
+    public static Settings settings;
+
+    public Keyz'_Misc_ResourcesMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from Keyz' Misc Resources");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.Keyz'_Misc_Resources.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Keyz' Misc Resources_SettingsCategory".Translate();
+    }
+}
